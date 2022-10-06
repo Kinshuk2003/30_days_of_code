0cae5494316b374689c79661b3a41b0e937456f1
Kinshuk Goyal <72309507+Kinshuk2003@users.noreply.github.com>
2022-10-06 19:35:51 +0530

    day 25 / 30

---
+++ Medium/Smallest number on left.py
@@ -0,0 +1,16 @@
+class Solution:
+    def leftSmaller(self, n, a):
+        stack=[]
+        ans=[]
+        for i in range(n):
+            while( stack !=[] and stack[-1]>=a[i]):
+                stack.pop()
+            
+            if stack==[]:
+                ans.append(-1)
+            else:
+                ans.append(stack[-1])
+            
+            stack.append(a[i])
+        
+        return ans
